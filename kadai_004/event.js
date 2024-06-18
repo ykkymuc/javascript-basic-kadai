bd34409e77e42188ebb7f79f6bb117ccc1a2b3c7
ykkymuc <161896268+ykkymuc@users.noreply.github.com>
2024-06-18 21:27:56 +0900

    フォルダをアップロードしました

---
+++ kadai_004/event.js
@@ -0,0 +1,10 @@
+
+  // HTMLをコードした時
+  $(window).on('load', function () {
+    console.log('loadイベントが発生しました');
+  });
+
+  // 画面をスクロールした時
+  $(window).on('scroll', function () {
+    console.log('scrollイベントが発生しました');
+  });
